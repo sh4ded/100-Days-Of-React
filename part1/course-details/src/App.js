449d8c66ced653c49e7dc7deb09c2b4ab120590b
sh4ded <threalsk@gmail.com>
2022-02-20 11:36:28 +0530

    Do Ex 1.1-1.2

--- part1/course-details/src/App.js
+++ part1/course-details/src/App.js
@@ -1,21 +1,39 @@
-const Hello = (props) => {
+const Header = (props) => {
   return (
-    <div>
-      <p>Hellow {props.name} world</p>
-    </div>
-  );
-};
+    <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+    )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+    )
+}
 
 const App = () => {
-  const name = "Sai";
-  const age = 19;
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <>
-      <Hello name="SaiK" />
-      <Hello name={name} />
+      <Header course={course} />
+      <Content part={part1} exercise={exercises1} />
+      <Content part={part2} exercise={exercises2} />
+      <Content part={part3} exercise={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
